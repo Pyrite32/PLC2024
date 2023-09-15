9cadb505e4525611320f8cbdc42f993e0f3158c3
Pyrite32 <patrick@method93.com>
2023-09-15 13:35:12 -0400

    adding lexer structure constants

--- edu/ufl/cise/cop4020fa23/Lexer.java
+++ edu/ufl/cise/cop4020fa23/Lexer.java
@@ -10,25 +10,55 @@
 package edu.ufl.cise.cop4020fa23;
 
 import static edu.ufl.cise.cop4020fa23.Kind.EOF;
-
+import edu.ufl.cise.cop4020fa23.LexicalStructure;
 import edu.ufl.cise.cop4020fa23.exceptions.LexicalException;
 
+import java.util.HashMap;
+
+import javax.lang.model.element.VariableElement;
 
 public class Lexer implements ILexer {
 
 	String input;
 
+	HashMap<String, Kind> literalToKind;
 
 	public Lexer(String input) {
 		this.input = input;
+		literalToKind = new HashMap<String, Kind>();
+	}
+
+	private void createLexicalStructure() {
+		// boolean literal
+		for (String s : LexicalStructure.BooleanLit) {
+			literalToKind.put(s, Kind.BOOLEAN_LIT);
+		}
+		// constant literal
+		for (String s : LexicalStructure.Constants) {
+			literalToKind.put(s, Kind.CONST);
+		}
+
 	}
 
 	@Override
 	public IToken next() throws LexicalException {
+		System.out.println("My contents are: ")
 		return new Token(EOF, 0, 0, null, new SourceLocation(1, 1));
+		printVar("input", input);
 	}
 
+	private void tellVar(VariableElement var) {
 
+	}
+
+	private static String getLoc() {
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+        if (stackTrace.length > 2) {
+            return stackTrace[2].getLineNumber();
+        } else {
+            return "?";
+        }
+    }
 
 
 }
---
+++ edu/ufl/cise/cop4020fa23/LexicalStructure.java
@@ -0,0 +1,82 @@
+package edu.ufl.cise.cop4020fa23;
+
+public final class LexicalStructure {
+    public static final String[] BooleanLit = {
+        "TRUE",
+        "FALSE"
+    };
+    public static final String[] Constants = {
+        "Z",
+        "BLACK",
+        "BLUE",
+        "CYAN",
+        "DARK_GRAY",
+        "GRAY",
+        "GREEN",
+        "LIGHT_GRAY",
+        "MAGENTA",
+        "ORANGE",
+        "PINK",
+        "RED",
+        "WHITE",
+        "YELLOW"
+    };
+
+    public static final String RES_Image = "image";
+    public static final String RES_Pixel = "pixel";
+    public static final String RES_Int = "int";
+    public static final String RES_String = "string";
+    public static final String RES_Void = "void";
+    public static final String RES_Boolean = "boolean";
+    public static final String RES_Write = "write";
+    public static final String RES_Height = "height";
+    public static final String RES_Width = "width";
+    public static final String RES_If = "if";
+    public static final String RES_Fi = "fi";
+    public static final String RES_Do = "do";
+    public static final String RES_Od = "od";
+    public static final String RES_Red = "red";
+    public static final String RES_Green = "green";
+    public static final String RES_Blue = "blue";
+    
+    public static final String Comma = ",";
+    public static final String Semi = ";";
+    public static final String Question = "?";
+    public static final String Colon = ":";
+    public static final String LParen = "(";
+    public static final String RParen = ")";
+    public static final String LT = "<";
+    public static final String GT = ">";
+    public static final String LSquare = "[";
+    public static final String RSquare = "]";
+    public static final String Assign = "=";
+    public static final String Bang = "!";
+    public static final String BitAnd = "&";
+    public static final String BitOr = "|";
+    public static final String Plus = "+";
+    public static final String OpMinus = "-";
+    public static final String OpTimes = "*";
+    public static final String Div = "/";
+    public static final String Mod = "%";
+    public static final String Return = "^";
+    
+    public static final String Eq = "==";
+    public static final String Le = "<=";
+    public static final String Ge = ">=";
+    public static final String And = "&&";
+    public static final String Or = "||";
+    public static final String Exp = "**";
+    public static final String BlockOpen = "<:";
+    public static final String BlockClose = ":>";
+    public static final String RArrow = "->";
+    public static final String Box = "[]";
+
+    public static final String[] WhiteSpace = {
+        " ",
+        "\n",
+        "\r"
+    };
+    public static final String[] CommentDelimiter = {
+        "##"
+    };
+}
